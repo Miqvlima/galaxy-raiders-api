2f3ce48974e75550aa272b53e3a7bbc145f16352
Miq <miqueias.vllima@usp.br>
2023-05-15 14:36:54 -0300

    Complete the code for Point2D.kt and Vector2D.kt

--- app/src/main/kotlin/galaxyraiders/core/physics/Point2D.kt
+++ app/src/main/kotlin/galaxyraiders/core/physics/Point2D.kt
@@ -3,11 +3,11 @@ package galaxyraiders.core.physics
 
 data class Point2D(val x: Double, val y: Double) {
   operator fun plus(p: Point2D): Point2D {
-    return INVALID_POINT
+    return Point2D(x + p.x, y + p.y)
   }
 
   operator fun plus(v: Vector2D): Point2D {
-    return INVALID_POINT
+    return Point2D(x + v.dx, y + v.dy)
   }
 
   override fun toString(): String {
@@ -15,26 +15,41 @@ data class Point2D(val x: Double, val y: Double) {
   }
 
   fun toVector(): Vector2D {
-    return INVALID_VECTOR
+    return Vector2D(x, y)
   }
 
   fun impactVector(p: Point2D): Vector2D {
-    return INVALID_VECTOR
+    return Vector2D(Math.abs(x - p.x), Math.abs(y - p.y))
   }
 
   fun impactDirection(p: Point2D): Vector2D {
-    return INVALID_VECTOR
+    
+    val x_value: Double = Math.abs(x - p.x)
+    val y_value: Double = Math.abs(y - p.y)
+    val impact_vector: Vector2D = Vector2D(x_value, y_value)
+    
+    return Vector2D(impact_vector.unit.dx, impact_vector.unit.dy)
   }
 
   fun contactVector(p: Point2D): Vector2D {
-    return INVALID_VECTOR
+    
+    val impact_vector: Vector2D = this.impactVector(p)
+    
+    return Vector2D(impact_vector.normal.dx, impact_vector.normal.dy)
   }
 
   fun contactDirection(p: Point2D): Vector2D {
-    return INVALID_VECTOR
+    
+    val contact_direction: Vector2D = this.impactVector(p)
+    
+    return Vector2D(contact_direction.normal.dx, contact_direction.normal.dy)
   }
 
   fun distance(p: Point2D): Double {
-    return INVALID_DOUBLE
+    
+    val horizontal_distance: Double = Math.abs(x - p.x)
+    val vertical_distance: Double = Math.abs(y - p.y)
+    
+    return Math.sqrt((horizontal_distance * horizontal_distance) + (vertical_distance* vertical_distance))
   }
 }
--- app/src/main/kotlin/galaxyraiders/core/physics/Vector2D.kt
+++ app/src/main/kotlin/galaxyraiders/core/physics/Vector2D.kt
@@ -10,57 +10,147 @@ data class Vector2D(val dx: Double, val dy: Double) {
   }
 
   val magnitude: Double
-    get() = INVALID_DOUBLE
+    get() {        
+     return Math.sqrt((this.dx*this.dx)+(this.dy*this.dy))
+    }
 
   val radiant: Double
-    get() = INVALID_DOUBLE
+    get(){
+    
+      var vector_magnitude: Double = this.magnitude
+      var cosine: Double = dx/vector_magnitude
+      var angle: Double = Math.acos(cosine)
+    
+      if (radiantQuadrant(angle) != vectorQuadrant(dx, dy)){
+        angle = -angle
+      }
+    
+      return angle      
+    } 
 
-  val degree: Double
-    get() = INVALID_DOUBLE
+  val degree: Double    
+    get(){
+    
+      val radians: Double = this.radiant
+    
+      return (radians*180)/Math.PI
+    }
 
   val unit: Vector2D
-    get() = INVALID_VECTOR
+    get(){
+    
+      val new_dx: Double = dx/this.magnitude
+      val new_dy: Double = dy/this.magnitude
+    
+      return Vector2D(new_dx, new_dy)
+    }
 
   val normal: Vector2D
-    get() = INVALID_VECTOR
+    get() {
+      return normalVector(this)
+    }
 
   operator fun times(scalar: Double): Vector2D {
-    return INVALID_VECTOR
+    return Vector2D(dx*scalar, dy*scalar)
   }
 
   operator fun div(scalar: Double): Vector2D {
-    return INVALID_VECTOR
+    return Vector2D(dx/scalar, dy/scalar)
   }
 
   operator fun times(v: Vector2D): Double {
-    return INVALID_DOUBLE
+    return (dx*v.dx + dy*v.dy)
   }
 
   operator fun plus(v: Vector2D): Vector2D {
-    return INVALID_VECTOR
+    return Vector2D(dx + v.dx, dy + v.dy)
   }
 
   operator fun plus(p: Point2D): Point2D {
-    return INVALID_POINT
+    return Point2D(this.dx + p.x, this.dy + p.y)
   }
 
   operator fun unaryMinus(): Vector2D {
-    return INVALID_VECTOR
+    return Vector2D(-dx, -dy)
   }
 
   operator fun minus(v: Vector2D): Vector2D {
-    return INVALID_VECTOR
+    return Vector2D(dx - v.dx, dy - v.dy)
   }
 
   fun scalarProject(target: Vector2D): Double {
-    return INVALID_DOUBLE
+    
+    val magnitude: Double = target.magnitude
+    val scalar_product: Double = dx*target.dx + dy*target.dy
+    
+    return scalar_product/magnitude 
   }
 
   fun vectorProject(target: Vector2D): Vector2D {
-    return INVALID_VECTOR
+    
+    val scalar_projection: Double = this.scalarProject(target)
+    
+    return scalar_projection * target.unit
   }
 }
 
 operator fun Double.times(v: Vector2D): Vector2D {
-  return INVALID_VECTOR
+  return Vector2D(this*v.dx, this*v.dy)
+}
+
+fun vectorQuadrant(x: Double, y: Double): Int {
+  if (x == 0.0){
+    if (y > 0)
+      return 2
+    else if (y < 0) 
+      return 4
+    else
+      return 1
+  }
+  else if (y == 0.0){
+    if (x > 0) 
+      return 1
+    else if (x < 0) 
+      return 3
+  }
+  else if ((x>0) && (y>0)){
+    return 1
+  }
+  else if ((x<0) && (y>0)){
+    return 2
+  }
+  else if((x<0) && (y<0)){
+    return 3
+  }
+  else if ((x>0) && (y<0)){
+    return 4
+  }
+  return 0
+}
+
+fun radiantQuadrant(angle: Double): Int {
+  if ((angle>=0) && (angle<(Math.PI/2))) return 1
+  else if ((angle>=(Math.PI/2)) && (angle<Math.PI)) return 2
+  else if ((angle<0) && (angle>=-(Math.PI/2))) return 4
+  else if ((angle>=-(Math.PI)) && (angle<-(Math.PI/2))) return 3
+  return 0
+}
+
+fun normalVector(vector: Vector2D): Vector2D {
+  
+  val vector_quadrant: Int = vectorQuadrant(vector.dx, vector.dy)
+  val normal_without_unit: Vector2D = Vector2D(1.0, -vector.dx/vector.dy)
+  var normal_vector: Vector2D = Vector2D(normal_without_unit.unit.dx, normal_without_unit.unit.dy)
+  val normal_quadrant: Int = vectorQuadrant(normal_vector.dx, normal_vector.dy)
+  
+  if ((vector_quadrant == 1) && (normal_quadrant == 4)){
+    return normal_vector
+  }
+  
+  else if (normal_quadrant == vector_quadrant - 1){
+    return normal_vector
+  }
+  
+  return -normal_vector
+
 }
